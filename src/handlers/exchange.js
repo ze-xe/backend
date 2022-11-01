47bd8ae1c39deee283b9ef8f2e3d5f1c4fb1ecb6
mubashirzebi <mubashshirzebi@gmail.com>
2022-11-01 11:20:47 +0530

    update order created

--- src/handlers/exchange.js
+++ src/handlers/exchange.js
@@ -1,4 +1,4 @@
-const { TokenWithdrawn, TokenDeposited, PairCreated, OrderCreated, OrderExecuted } = require("../db");
+const { TokenWithdrawn, TokenDeposited, PairCreated, OrderCreated, OrderExecuted, UserPosition } = require("../db");
 const { tronWeb } = require("../utils");
 const { handleToken } = require("./token");
 
@@ -50,15 +50,64 @@ async function handleOrderCreated(data, argument) {
             return
         }
 
-        argument.id = data[0];
-        argument.pair = data[1];
-        argument.maker = tronWeb.address.fromHex(data[2])
-        argument.amount = data[3];
-        argument.exchangeRate = data[4];
-        argument.orderType = data[5];
+        let id = data[0];
+        let pair = data[1];
+        let maker = tronWeb.address.fromHex(data[2]);
+        let amount = data[3];
+        let exchangeRate = data[4];
+        let orderType = data[5];
+        argument.id = id;
+        argument.pair = pair;
+        argument.maker = maker;
+        argument.amount = amount;
+        argument.exchangeRate = exchangeRate;
+        argument.orderType = orderType;
 
         OrderCreated.create(argument);
-        console.log("Order Created", tronWeb.address.fromHex(data[2]), data[3])
+
+        if (data[5] == '0') {
+
+            let findToken0 = await PairCreated.findOne({ id: pair });
+
+            let token0 = findToken0.token0;
+
+            let findUserPosition = await UserPosition.findOne({ id : maker, token: token0 });
+
+            let _id = findUserPosition._id.toString();
+
+            let currentInOrderBalance = Number(findUserPosition.InOrderBalance ?? 0) + Number(amount);
+
+            let currentBalance = Number(findUserPosition.balance) - Number(amount);
+
+            await UserPosition.updateOne(
+                { _id: _id },
+                { $set: { InOrderBalance: currentInOrderBalance, balance: currentBalance } }
+            );
+
+        }
+        else if (data[5] == '1') {
+
+            let findToken1 = await PairCreated.findOne({ id: pair });
+
+            let token1 = findToken1.token1;
+
+            let findUserPosition = await UserPosition.findOne({ id: maker, token: token1 });
+
+            let token1Amount = (Number(amount) * (exchangeRate)) / (10 ** Number(findToken1.exchangeRateDecimals));
+
+            let currentInOrderBalance = Number(findUserPosition.InOrderBalance ?? 0) + token1Amount;
+
+            let _id = findUserPosition._id.toString();
+
+            let currentBalance = Number(findUserPosition.balance) - token1Amount;
+
+            await UserPosition.updateOne(
+                { _id: _id },
+                { $set: { InOrderBalance: currentInOrderBalance, balance: currentBalance } }
+            )
+        }
+
+        console.log("Order Created", maker, amount);
 
     }
     catch (error) {
